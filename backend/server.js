4744d163fc9959d4593d335bcc7d0a40e73729da
Asad Ghaffar <fa21-bcs-015@isbstudent.comsats.edu.pk>
2025-11-04 11:46:39 +0500

    fix: weather route fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import validateTokenRoutes from './routes/validateTokenRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import farmingNewsRoute from './routes/farmingNewsRoute.js';
 import marketRoutes from './routes/marketRoutes.js';
-import weatherRoutes from './routes/weatherRoutes.js';
 import weatherRoute from './routes/weatherRoute.js';
 import shortAdviceRoute from './routes/shortAdviceRoute.js';
 
@@ -30,7 +29,6 @@ app.use('/api/auth', validateTokenRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/news', farmingNewsRoute);
 app.use('/api/market', marketRoutes);
-app.use('/api/weather', weatherRoutes);
 app.use('/api/weather', weatherRoute);
 app.use('/api/short-advice', shortAdviceRoute);
 
